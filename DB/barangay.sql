d4f7fab794c6abf254225aaac0f38d552aa582cd
unknown <silent.kevin.f.caluag20@gmail.com>
2021-05-19 20:04:29 -0700

    asd

---
+++ DB/barangay.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2021 at 05:03 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `barangay`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `residents information`
+--
+
+CREATE TABLE `residents information` (
+  `First Name` varchar(100) NOT NULL,
+  `Middle Name` varchar(100) NOT NULL,
+  `Last Name` varchar(100) NOT NULL,
+  `Gender` varchar(100) NOT NULL,
+  `Date Of Birth` varchar(100) NOT NULL,
+  `Place Of Birth` varchar(100) NOT NULL,
+  `Civil Status` varchar(100) NOT NULL,
+  `Citizenship` varchar(100) NOT NULL,
+  `Religion` varchar(100) NOT NULL,
+  `Occupation` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
